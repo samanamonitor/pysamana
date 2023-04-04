9ef052d7f02bfb52c3b417ea33a5f0649608f27f
Fabian Baena <fabbaena@gmail.com>
2023-04-04 10:48:56 -0400

    separate version

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+import version
 
 setup(
     name='samana',
-    version='0.0.1',
+    version=version.VERSION,
     packages=find_packages(include=['samana', 'samana.*']),
     install_requires=[ 'urllib3' ]
 )
