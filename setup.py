cb175f38386ed81e165982be13f7c0cc0adbe9f3
Fabian Baena <fabbaena@gmail.com>
2023-04-04 11:08:40 -0400

    remove version file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import version
 
 setup(
     name='samana',
-    version='0.0.1,
+    version='0.0.1',
     packages=find_packages(include=['samana', 'samana.*']),
     install_requires=[ 'urllib3' ]
 )
