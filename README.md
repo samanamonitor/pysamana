ca16e9d0ea45d65b1bff3d6b78e68257c2facbe2
Fabian Baena <fabbaena@gmail.com>
2023-04-10 10:27:50 -0400

    added debian package info

---
+++ README.md
@@ -0,0 +1,5 @@
+# PYSAMMWR package
+This package contains Samana Check libraries
+
+# To compile
+docker run -it --rm --mount type=bind,source=$(pwd),target=/usr/src/pysamana -w /usr/src/pysamana ubuntu:bionic /bin/bash
--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
+from samana import __version__
 
 setup(
     name='samana',
-    version='0.0.1',
+    version=__version__,
     packages=find_packages(include=['samana', 'samana.*']),
     install_requires=[ 'urllib3' ]
 )
